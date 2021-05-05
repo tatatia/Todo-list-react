357877283330f315ca5c3de98af9f338c79834d6
tatatia <thetattianochka@gmail.com>
2021-05-05 20:13:16 +0300

    creating components and using useState

---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Todo from "./Todo"
+
+const TodoList = ({todos}) => {
+    console.log(todos)
+    return (
+        <div className="todo-container">
+            <ul className="todo-list">
+                <Todo />
+            </ul>
+        </div>
+    )
+}
+export default TodoList
